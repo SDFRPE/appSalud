c71749bef343721b042d408744b9a8d0642c1c09
Shande Rojas <sfernandezro01@ucvvirtual.edu.pe>
2026-07-01 13:30:40 -0500

    Co-authored-by: LeonardoRRC <128936819+LeonardoRRC@users.noreply.github.com>

--- public/test_env.php
+++ public/test_env.php
@@ -1,7 +1,7 @@
 <?php
 require_once '../config/env.php';
 loadEnv(__DIR__ . '/../.env');
-
+s
 echo 'DB_HOST: ' . $_ENV['DB_HOST'] . '<br>';
 echo 'DB_NAME: ' . $_ENV['DB_NAME'] . '<br>';
 echo 'DB_USERNAME: ' . $_ENV['DB_USERNAME'] . '<br>';
